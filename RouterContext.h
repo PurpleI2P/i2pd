7f08f782e062610223ff97b3e95150eac6b7eeae
orignal <romakoshelkin@yandex.ru>
2013-12-12 21:37:58 -0500

    LeaseSet keys

--- RouterContext.h
+++ RouterContext.h
@@ -20,6 +20,8 @@ namespace i2p
 			i2p::data::RouterInfo& GetRouterInfo () { return m_RouterInfo; };
 			const uint8_t * GetPrivateKey () const { return m_PrivateKey; };
 			const uint8_t * GetSigningPrivateKey () const;
+			const uint8_t * GetLeaseSetPrivateKey () const { return m_LeaseSetPrivateKey; };
+			const uint8_t * GetLeaseSetPublicKey () const { return m_LeaseSetPublicKey; };
 			const i2p::data::RouterIdentity& GetRouterIdentity () const { return m_RouterInfo.GetRouterIdentity (); };
 			CryptoPP::RandomNumberGenerator& GetRandomNumberGenerator () { return m_Rnd; };	
 			
@@ -37,7 +39,8 @@ namespace i2p
 
 			i2p::data::RouterInfo m_RouterInfo;
 			CryptoPP::DSA::PrivateKey m_SigningPrivateKey;
-			uint8_t m_PrivateKey[256], m_SigningPrivateKeyStr[20];
+			uint8_t m_PrivateKey[256], m_SigningPrivateKeyStr[20], 
+				m_LeaseSetPublicKey[256], m_LeaseSetPrivateKey[256];
 			CryptoPP::AutoSeededRandomPool m_Rnd;
 	};
 
