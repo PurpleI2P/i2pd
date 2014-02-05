728c1a9226109c1449297d6f2f293040d1889ad2
chertov <chertovmv@gmail.com>
2014-02-05 09:14:07 +0400

    fix LittleEndian, change endian.h version

--- I2PEndian.h
+++ I2PEndian.h
@@ -5,16 +5,14 @@
 #include <endian.h>
 #else
 #include <cstdint>
-//
-//uint16_t htobe16(uint16_t int16);
-//uint32_t htobe32(uint32_t int32);
-//uint64_t htobe64(uint64_t int64);
-//
-//uint16_t be16toh(uint16_t big16);
-//uint32_t be32toh(uint32_t big32);
-//uint64_t be64toh(uint64_t big64);
 
-#include "portable_endian.h"
+uint16_t htobe16(uint16_t int16);
+uint32_t htobe32(uint32_t int32);
+uint64_t htobe64(uint64_t int64);
+
+uint16_t be16toh(uint16_t big16);
+uint32_t be32toh(uint32_t big32);
+uint64_t be64toh(uint64_t big64);
 
 #endif
 
--- Streaming.cpp
+++ Streaming.cpp
@@ -2,7 +2,6 @@
 #include <algorithm>
 #include <cryptopp/gzip.h>
 #include "Log.h"
-#include "I2PEndian.h"
 #include "RouterInfo.h"
 #include "RouterContext.h"
 #include "Tunnel.h"
