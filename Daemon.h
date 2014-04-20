0b210ee99d9784cfd33d9db9a6a46c235b8f6559
chertov <chertovmv@gmail.com>
2014-04-20 05:54:34 +0400

    Add simple Linux, Win32 daemons

---
+++ Daemon.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "HTTPServer.h"
+#include "HTTPProxy.h"
+
+namespace i2p
+{
+	namespace util
+	{
+		class Daemon_Singleton
+		{
+		public:
+			virtual bool start();
+			virtual bool stop();
+
+			int isLogging;
+			int isDaemon;
+			
+			int running = 1;
+
+		private:
+			i2p::util::HTTPServer *httpServer;
+			i2p::proxy::HTTPProxy *httpProxy;
+
+		protected:
+			Daemon_Singleton() : running(1) {};
+			virtual ~Daemon_Singleton() {
+				delete httpServer;
+				delete httpProxy;
+			};
+		};
+
+#ifdef _WIN32
+		#define Daemon i2p::util::DaemonWin32::Instance()
+		class DaemonWin32 : public Daemon_Singleton
+		{
+		public:
+			static DaemonWin32& Instance()
+			{
+				static DaemonWin32 instance;
+				return instance;
+			}
+
+			virtual bool start();
+			virtual bool stop();
+		};
+#else
+		#define Daemon i2p::util::DaemonLinux::Instance()
+		class DaemonLinux : public Daemon_Singleton
+		{
+		public:
+			static DaemonLinux& Instance()
+			{
+				static DaemonLinux instance;
+				return instance;
+			}
+
+			virtual bool start();
+			virtual bool stop();
+		};
+#endif
+	}
+}
---
+++ DaemonLinux.cpp
@@ -0,0 +1,109 @@
+#include "Daemon.h"
+
+#ifndef _WIN32
+
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void handle_signal(int sig)
+{
+	switch (sig)
+	{
+	case SIGHUP:
+		if (i2p::util::config::GetArg("daemon", 0) == 1)
+		{
+			static bool first=true;
+			if (first)
+			{
+				first=false;
+				return;
+			}
+		}
+		LogPrint("Reloading config.");
+		i2p::util::filesystem::ReadConfigFile(i2p::util::config::mapArgs, i2p::util::config::mapMultiArgs);
+		break;
+	case SIGABRT:
+	case SIGTERM:
+	case SIGINT:
+		running = 0; // Exit loop
+		break;
+	}
+}
+
+
+namespace i2p
+{
+	namespace util
+	{
+		bool DaemonLinux::start()
+		{
+			if (isDaemon == 1)
+			{
+				pid_t pid;
+				pid = fork();
+				if (pid > 0)
+				{
+					g_Log.Stop();
+					return 0;
+				}
+				if (pid < 0)
+				{
+					return -1;
+				}
+
+				umask(0);
+				int sid = setsid();
+				if (sid < 0)
+				{
+					LogPrint("Error, could not create process group.");
+					return -1;
+				}
+				chdir(i2p::util::filesystem::GetDataDir().string().c_str());
+			}
+
+			// Pidfile
+			std::string pidfile = i2p::util::filesystem::GetDataDir().string();
+			pidfile.append("/i2pd.pid");
+			int pidFilehandle = open(pidfile.c_str(), O_RDWR | O_CREAT, 0600);
+			if (pidFilehandle == -1)
+			{
+				LogPrint("Error, could not create pid file (", pidfile, ")\nIs an instance already running?");
+				return -1;
+			}
+			if (lockf(pidFilehandle, F_TLOCK, 0) == -1)
+			{
+				LogPrint("Error, could not lock pid file (", pidfile, ")\nIs an instance already running?");
+				return -1;
+			}
+			char pid[10];
+			sprintf(pid, "%d\n", getpid());
+			write(pidFilehandle, pid, strlen(pid));
+
+			// Signal handler
+			struct sigaction sa;
+			sa.sa_handler = handle_signal;
+			sigemptyset(&sa.sa_mask);
+			sa.sa_flags = SA_RESTART;
+			sigaction(SIGHUP, &sa, 0);
+			sigaction(SIGABRT, &sa, 0);
+			sigaction(SIGTERM, &sa, 0);
+			sigaction(SIGINT, &sa, 0);
+
+			retunrn Daemon_Singleton::start();
+		}
+
+		bool DaemonLinux::stop()
+		{
+			Daemon_Singleton::stop();
+
+			close(pidFilehandle);
+			unlink(pidfile.c_str());
+
+			return true;
+		}
+
+	}
+}
+
+#endif
