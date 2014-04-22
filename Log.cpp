0236879c2605f34b50f8ba8e2a896d6519fe2fcb
chertov <chertovmv@gmail.com>
2014-04-23 02:10:21 +0400

    Logging to file directly. Win32 Services can't write to std::cout

--- Log.cpp
+++ Log.cpp
@@ -1,3 +1,15 @@
 #include "Log.h"
 
+#include "Daemon.h"
+
 i2p::util::MsgQueue<LogMsg> g_Log;
+
+void LogMsg::Process()
+{
+	if (Daemon.isLogging == 1 && Daemon.logfile.is_open())
+	{
+		Daemon.logfile << s.str();
+		Daemon.logfile.flush();
+	}
+	output << s.str();
+}
