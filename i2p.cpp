64ba1622c2eb89911a9a66d5e61fe0a742a6e63c
chertov <chertovmv@gmail.com>
2014-04-23 02:10:51 +0400

    Use Daemon.init in main()

--- i2p.cpp
+++ i2p.cpp
@@ -1,16 +1,10 @@
-#include <boost/filesystem.hpp>
+#include <thread>
 
-#include "util.h"
 #include "Daemon.h"
 
 int main( int argc, char* argv[] )
 {
-	i2p::util::config::OptionParser(argc, argv);
-
-	LogPrint("\n\n\n\ni2pd starting\n");
-	LogPrint("data directory: ", i2p::util::filesystem::GetDataDir().string());
-	i2p::util::filesystem::ReadConfigFile(i2p::util::config::mapArgs, i2p::util::config::mapMultiArgs);
-
+	Daemon.init(argc, argv);
 	Daemon.start();
 	while (Daemon.running)
 	{
@@ -18,6 +12,5 @@ int main( int argc, char* argv[] )
 		std::this_thread::sleep_for (std::chrono::seconds(1));
 	}
 	Daemon.stop();
-
-	return 0;
+	return EXIT_SUCCESS;
 }
