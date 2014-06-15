9ecf2500f508798871ba8de92ded1e3dc3599332
Mikal Villa <meeh@sigterm.no>
2014-06-15 13:03:29 +0200

    Adding support for Mac OSX.
    Compiles fine on 10.9,
    $ uname -a
    Darwin mikalv.local 13.1.0 Darwin Kernel Version 13.1.0: Thu Jan 16 19:40:37 PST
    2014; root:xnu-2422.90.20~2/RELEASE_X86_64 x86_64

--- I2PEndian.h
+++ I2PEndian.h
@@ -5,11 +5,27 @@
 #include <endian.h>
 #elif __FreeBSD__
 #include <sys/endian.h>
-#elif __MACH__ // Mac OS X
-#include <machine/endian.h>
+#elif defined(__APPLE__) && defined(__MACH__)
+
+#include <libkern/OSByteOrder.h>
+
+#define htobe16(x) OSSwapHostToBigInt16(x)
+#define htole16(x) OSSwapHostToLittleInt16(x)
+#define be16toh(x) OSSwapBigToHostInt16(x)
+#define le16toh(x) OSSwapLittleToHostInt16(x)
+
+#define htobe32(x) OSSwapHostToBigInt32(x)
+#define htole32(x) OSSwapHostToLittleInt32(x)
+#define be32toh(x) OSSwapBigToHostInt32(x)
+#define le32toh(x) OSSwapLittleToHostInt32(x)
+
+#define htobe64(x) OSSwapHostToBigInt64(x)
+#define htole64(x) OSSwapHostToLittleInt64(x)
+#define be64toh(x) OSSwapBigToHostInt64(x)
+#define le64toh(x) OSSwapLittleToHostInt64(x)
+
 #else
 #include <cstdint>
-
 uint16_t htobe16(uint16_t int16);
 uint32_t htobe32(uint32_t int32);
 uint64_t htobe64(uint64_t int64);
@@ -21,3 +37,4 @@ uint64_t be64toh(uint64_t big64);
 #endif
 
 #endif // I2PENDIAN_H__
+
