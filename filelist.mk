02296ef775fa4afe5d4049906fac0b99bae14b8d
Mikal Villa <meeh@sigterm.no>
2014-07-17 19:12:52 +0200

    Moving file list to a common one. Still in makefile format, but now with CPP/H as input instead of OBJECTS. Issue #82

---
+++ filelist.mk
@@ -0,0 +1,18 @@
+
+
+CPP_FILES := CryptoConst.cpp base64.cpp NTCPSession.cpp RouterInfo.cpp Transports.cpp \
+	RouterContext.cpp NetDb.cpp LeaseSet.cpp Tunnel.cpp TunnelEndpoint.cpp TunnelGateway.cpp \
+	TransitTunnel.cpp I2NPProtocol.cpp Log.cpp Garlic.cpp HTTPServer.cpp Streaming.cpp Identity.cpp \
+	SSU.cpp util.cpp Reseed.cpp DaemonLinux.cpp SSUData.cpp i2p.cpp aes.cpp SOCKS.cpp UPnP.cpp \
+	TunnelPool.cpp HTTPProxy.cpp AddressBook.cpp Daemon.cpp
+
+
+H_FILES := CryptoConst.h base64.h NTCPSession.h RouterInfo.h Transports.h \
+	RouterContext.h NetDb.h LeaseSet.h Tunnel.h TunnelEndpoint.h TunnelGateway.h \
+	TransitTunnel.h I2NPProtocol.h Log.h Garlic.h HTTPServer.h Streaming.h Identity.h \
+	SSU.h util.h Reseed.h DaemonLinux.h SSUData.h i2p.h aes.h SOCKS.h UPnP.h TunnelPool.h \
+	HTTPProxy.h AddressBook.h Daemon.h
+
+
+OBJECTS = $(addprefix obj/, $(notdir $(CPP_FILES:.cpp=.o)))
+
