cb06f8e0bb44d35ac47e91a42e790e015eb61ebe
orignal <i2porignal@yandex.ru>
2014-12-08 19:33:50 -0500

    enale tunnl test encryption back

--- TunnelPool.cpp
+++ TunnelPool.cpp
@@ -208,10 +208,16 @@ namespace tunnel
 			}
 			if (!failed)
 			{
-				uint32_t msgID = rnd.GenerateWord32 ();
-				m_Tests[msgID] = std::make_pair (*it1, *it2);
-				(*it1)->SendTunnelDataMsg ((*it2)->GetNextIdentHash (), (*it2)->GetNextTunnelID (),
-					CreateDeliveryStatusMsg (msgID));
+				uint8_t key[32], tag[32];
+				rnd.GenerateBlock (key, 32); // random session key 
+				rnd.GenerateBlock (tag, 32); // random session tag
+				m_LocalDestination.SubmitSessionKey (key, tag);
+				i2p::garlic::GarlicRoutingSession garlic (key, tag);
+
+ 				uint32_t msgID = rnd.GenerateWord32 ();
+ 				m_Tests[msgID] = std::make_pair (*it1, *it2);
+ 				(*it1)->SendTunnelDataMsg ((*it2)->GetNextIdentHash (), (*it2)->GetNextTunnelID (),
+					garlic.WrapSingleMessage (CreateDeliveryStatusMsg (msgID)));
 				it1++; it2++;
 			}	
 		}
