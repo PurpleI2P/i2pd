24564f1faad1bf6094fe18d9b96b4caf42c6ea5f
orignal <i2porignal@yandex.ru>
2015-02-02 09:58:34 -0500

    moved UPNP to common files

--- filelist.mk
+++ filelist.mk
@@ -3,7 +3,7 @@ COMMON_SRC = \
   LeaseSet.cpp Log.cpp NTCPSession.cpp NetDb.cpp Reseed.cpp RouterContext.cpp \
   RouterInfo.cpp SSU.cpp SSUSession.cpp SSUData.cpp Streaming.cpp Identity.cpp \
   TransitTunnel.cpp Transports.cpp Tunnel.cpp TunnelEndpoint.cpp TunnelPool.cpp \
-  TunnelGateway.cpp Destination.cpp util.cpp aes.cpp base64.cpp
+  TunnelGateway.cpp Destination.cpp UPnP.cpp util.cpp aes.cpp base64.cpp
 
 
 ifeq ($(UNAME),Darwin)
@@ -17,7 +17,7 @@ endif
 
 # also: Daemon{Linux,Win32}.cpp will be added later
 DAEMON_SRC = $(COMMON_SRC) \
-  BOB.cpp ClientContext.cpp Daemon.cpp I2PTunnel.cpp I2PService.cpp SAM.cpp SOCKS.cpp UPnP.cpp \
+  BOB.cpp ClientContext.cpp Daemon.cpp I2PTunnel.cpp I2PService.cpp SAM.cpp SOCKS.cpp \
   HTTPServer.cpp HTTPProxy.cpp I2PControl.cpp i2p.cpp
 
 LIB_SRC := $(COMMON_SRC) \
