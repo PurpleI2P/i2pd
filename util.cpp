edd9a18257a0dac39cdf4d2a75468d4620d496bb
Mikhail Titov <mlt@gmx.us>
2015-12-12 18:11:48 -0600

    Cleanup some CMake msys specifics
    * Exclude MSYS from -z relro
    * WIN32_LEAN_AND_MEAN

--- util.cpp
+++ util.cpp
@@ -28,7 +28,9 @@
 #include <iphlpapi.h>
 #include <shlobj.h>
 
+#ifdef _MSC_VER
 #pragma comment(lib, "IPHLPAPI.lib")
+#endif
 
 #define MALLOC(x) HeapAlloc(GetProcessHeap(), 0, (x))
 #define FREE(x) HeapFree(GetProcessHeap(), 0, (x))
