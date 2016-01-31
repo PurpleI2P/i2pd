8805f1e4d64cea5bc2b94a1d6e3ab63b5c83fdfd
h0bbyte <hobbyte@neko.rocks>
2016-01-31 22:52:20 +0300

    I2PControl add total.sent|received.bytes

--- I2PControl.h
+++ I2PControl.h
@@ -20,56 +20,58 @@ namespace i2p
 namespace client
 {
 	const size_t I2P_CONTROL_MAX_REQUEST_SIZE = 1024;
-	typedef std::array<char, I2P_CONTROL_MAX_REQUEST_SIZE> I2PControlBuffer;		
+	typedef std::array<char, I2P_CONTROL_MAX_REQUEST_SIZE> I2PControlBuffer;
 
 	const char I2P_CONTROL_PATH[] = "ipcontrol";
 	const char I2P_CONTROL_KEY_FILE[] = "key.pem";
 	const char I2P_CONTROL_CERT_FILE[] = "cert.pem";
 	const char I2P_CONTROL_CONFIG_FILE[] = "i2pcontrol.conf";
-	const char I2P_CONTROL_DEFAULT_PASSWORD[] = "itoopie";	
+	const char I2P_CONTROL_DEFAULT_PASSWORD[] = "itoopie";
 
 	const char I2P_CONTROL_PROPERTY_ID[] = "id";
 	const char I2P_CONTROL_PROPERTY_METHOD[] = "method";
 	const char I2P_CONTROL_PROPERTY_PARAMS[] = "params";
 	const char I2P_CONTROL_PROPERTY_RESULT[] = "result";
 
-	// methods	
+	// methods
 	const char I2P_CONTROL_METHOD_AUTHENTICATE[] = "Authenticate";
 	const char I2P_CONTROL_METHOD_ECHO[] = "Echo";
-	const char I2P_CONTROL_METHOD_I2PCONTROL[] = "I2PControl";		
-	const char I2P_CONTROL_METHOD_ROUTER_INFO[] = "RouterInfo";	
-	const char I2P_CONTROL_METHOD_ROUTER_MANAGER[] = "RouterManager";	
-	const char I2P_CONTROL_METHOD_NETWORK_SETTING[] = "NetworkSetting";	
+	const char I2P_CONTROL_METHOD_I2PCONTROL[] = "I2PControl";
+	const char I2P_CONTROL_METHOD_ROUTER_INFO[] = "RouterInfo";
+	const char I2P_CONTROL_METHOD_ROUTER_MANAGER[] = "RouterManager";
+	const char I2P_CONTROL_METHOD_NETWORK_SETTING[] = "NetworkSetting";
 
 	// params
-	const char I2P_CONTROL_PARAM_API[] = "API";			
-	const char I2P_CONTROL_PARAM_PASSWORD[] = "Password";	
-	const char I2P_CONTROL_PARAM_TOKEN[] = "Token";	
-	const char I2P_CONTROL_PARAM_ECHO[] = "Echo";	
-	const char I2P_CONTROL_PARAM_RESULT[] = "Result";	
+	const char I2P_CONTROL_PARAM_API[] = "API";
+	const char I2P_CONTROL_PARAM_PASSWORD[] = "Password";
+	const char I2P_CONTROL_PARAM_TOKEN[] = "Token";
+	const char I2P_CONTROL_PARAM_ECHO[] = "Echo";
+	const char I2P_CONTROL_PARAM_RESULT[] = "Result";
 
 	// I2PControl
-	const char I2P_CONTROL_I2PCONTROL_ADDRESS[] = "i2pcontrol.address";		
+	const char I2P_CONTROL_I2PCONTROL_ADDRESS[] = "i2pcontrol.address";
 	const char I2P_CONTROL_I2PCONTROL_PASSWORD[] = "i2pcontrol.password";
-	const char I2P_CONTROL_I2PCONTROL_PORT[] = "i2pcontrol.port";		
+	const char I2P_CONTROL_I2PCONTROL_PORT[] = "i2pcontrol.port";
 
 	// RouterInfo requests
 	const char I2P_CONTROL_ROUTER_INFO_UPTIME[] = "i2p.router.uptime";
 	const char I2P_CONTROL_ROUTER_INFO_VERSION[] = "i2p.router.version";
-	const char I2P_CONTROL_ROUTER_INFO_STATUS[] = "i2p.router.status";	
+	const char I2P_CONTROL_ROUTER_INFO_STATUS[] = "i2p.router.status";
 	const char I2P_CONTROL_ROUTER_INFO_NETDB_KNOWNPEERS[] = "i2p.router.netdb.knownpeers";
 	const char I2P_CONTROL_ROUTER_INFO_NETDB_ACTIVEPEERS[] = "i2p.router.netdb.activepeers";
-	const char I2P_CONTROL_ROUTER_INFO_NET_STATUS[] = "i2p.router.net.status";	
+	const char I2P_CONTROL_ROUTER_INFO_NET_STATUS[] = "i2p.router.net.status";
 	const char I2P_CONTROL_ROUTER_INFO_TUNNELS_PARTICIPATING[] = "i2p.router.net.tunnels.participating";
-	const char I2P_CONTROL_ROUTER_INFO_BW_IB_1S[] = "i2p.router.net.bw.inbound.1s";			
-	const char I2P_CONTROL_ROUTER_INFO_BW_OB_1S[] = "i2p.router.net.bw.outbound.1s";		
+	const char I2P_CONTROL_ROUTER_INFO_BW_IB_1S[] = "i2p.router.net.bw.inbound.1s";
+	const char I2P_CONTROL_ROUTER_INFO_BW_OB_1S[] = "i2p.router.net.bw.outbound.1s";
+	const char I2P_CONTROL_ROUTER_INFO_NET_TOTAL_RB[] = "i2p.router.net.total.received.bytes";
+	const char I2P_CONTROL_ROUTER_INFO_NET_TOTAL_SB[] = "i2p.router.net.total.sent.bytes";
 
 	// RouterManager requests
 	const char I2P_CONTROL_ROUTER_MANAGER_SHUTDOWN[] = "Shutdown";
 	const char I2P_CONTROL_ROUTER_MANAGER_SHUTDOWN_GRACEFUL[] = "ShutdownGraceful";
-	const char I2P_CONTROL_ROUTER_MANAGER_RESEED[] = "Reseed";		
+	const char I2P_CONTROL_ROUTER_MANAGER_RESEED[] = "Reseed";
 
-	// Certificate	
+	// Certificate
 	const long I2P_CONTROL_CERTIFICATE_VALIDITY = 365*10; // 10 years
 	const char I2P_CONTROL_CERTIFICATE_COMMON_NAME[] = "i2pd.i2pcontrol";
 	const char I2P_CONTROL_CERTIFICATE_ORGANIZATION[] = "Purple I2P";
@@ -92,11 +94,11 @@ namespace client
 
 			void Run ();
 			void Accept ();
-			void HandleAccept(const boost::system::error_code& ecode, std::shared_ptr<ssl_socket> socket);	
+			void HandleAccept(const boost::system::error_code& ecode, std::shared_ptr<ssl_socket> socket);
 			void Handshake (std::shared_ptr<ssl_socket> socket);
 			void HandleHandshake (const boost::system::error_code& ecode, std::shared_ptr<ssl_socket> socket);
 			void ReadRequest (std::shared_ptr<ssl_socket> socket);
-			void HandleRequestReceived (const boost::system::error_code& ecode, size_t bytes_transferred, 
+			void HandleRequestReceived (const boost::system::error_code& ecode, size_t bytes_transferred,
 				std::shared_ptr<ssl_socket> socket, std::shared_ptr<I2PControlBuffer> buf);
 			void SendResponse (std::shared_ptr<ssl_socket> socket,
 				std::shared_ptr<I2PControlBuffer> buf, std::ostringstream& response, bool isHtml);
@@ -105,7 +107,7 @@ namespace client
 
 			boost::filesystem::path GetPath () const { return i2p::util::filesystem::GetDefaultDataDir() / I2P_CONTROL_PATH; };
 			void CreateCertificate ();
-			
+
 
 		private:
 
@@ -121,7 +123,7 @@ namespace client
 			void I2PControlHandler (const boost::property_tree::ptree& params, std::ostringstream& results);
 			void RouterInfoHandler (const boost::property_tree::ptree& params, std::ostringstream& results);
 			void RouterManagerHandler (const boost::property_tree::ptree& params, std::ostringstream& results);
-			void NetworkSettingHandler (const boost::property_tree::ptree& params, std::ostringstream& results);			
+			void NetworkSettingHandler (const boost::property_tree::ptree& params, std::ostringstream& results);
 
 			// I2PControl
 			typedef void (I2PControlService::*I2PControlRequestHandler)(const std::string& value);
@@ -133,11 +135,13 @@ namespace client
 			void VersionHandler (std::ostringstream& results);
 			void StatusHandler (std::ostringstream& results);
 			void NetDbKnownPeersHandler (std::ostringstream& results);
-			void NetDbActivePeersHandler (std::ostringstream& results);	
-			void NetStatusHandler (std::ostringstream& results);		
+			void NetDbActivePeersHandler (std::ostringstream& results);
+			void NetStatusHandler (std::ostringstream& results);
 			void TunnelsParticipatingHandler (std::ostringstream& results);
 			void InboundBandwidth1S (std::ostringstream& results);
 			void OutboundBandwidth1S (std::ostringstream& results);
+			void NetTotalReceivedBytes (std::ostringstream& results);
+			void NetTotalSentBytes (std::ostringstream& results);
 
 			// RouterManager
 			typedef void (I2PControlService::*RouterManagerRequestHandler)(std::ostringstream& results);
@@ -146,20 +150,20 @@ namespace client
 			void ReseedHandler (std::ostringstream& results);
 
 			// NetworkSetting
-			typedef void (I2PControlService::*NetworkSettingRequestHandler)(const std::string& value, std::ostringstream& results);	
+			typedef void (I2PControlService::*NetworkSettingRequestHandler)(const std::string& value, std::ostringstream& results);
 
 		private:
 
 			std::string m_Password;
 			bool m_IsRunning;
-			std::thread * m_Thread;	
+			std::thread * m_Thread;
 
 			boost::asio::io_service m_Service;
 			boost::asio::ip::tcp::acceptor m_Acceptor;
 			boost::asio::ssl::context m_SSLContext;
 			boost::asio::deadline_timer m_ShutdownTimer;
 			std::set<std::string> m_Tokens;
-			
+
 			std::map<std::string, MethodHandler> m_MethodHandlers;
 			std::map<std::string, I2PControlRequestHandler> m_I2PControlHandlers;
 			std::map<std::string, RouterInfoRequestHandler> m_RouterInfoHandlers;
