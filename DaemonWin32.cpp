74d4b8e0b92cd9dc2f7055cb7296ce441c15269f
orignal <i2porignal@yandex.ru>
2016-03-09 14:41:14 -0500

    invoke win32app functions from main

--- DaemonWin32.cpp
+++ DaemonWin32.cpp
@@ -5,7 +5,7 @@
 
 #ifdef _WIN32
 
-#include "./Win32/Win32Service.h"
+#include "Win32/Win32App.h"
 
 namespace i2p
 {
@@ -17,60 +17,20 @@ namespace i2p
 			SetConsoleCP(1251);
 			SetConsoleOutputCP(1251);
 			setlocale(LC_ALL, "Russian");
-
-			if (!Daemon_Singleton::init(argc, argv)) return false;
-			if (I2PService::isService())
-				isDaemon = 1;
-			else
-				isDaemon = 0;
-
-			std::string serviceControl; i2p::config::GetOption("svcctl", serviceControl);
-			if (serviceControl == "install")
-			{
-				LogPrint(eLogInfo, "WinSVC: installing ", SERVICE_NAME, " as service");
-				InstallService(
-					SERVICE_NAME,               // Name of service
-					SERVICE_DISPLAY_NAME,       // Name to display
-					SERVICE_START_TYPE,         // Service start type
-					SERVICE_DEPENDENCIES,       // Dependencies
-					SERVICE_ACCOUNT,            // Service running account
-					SERVICE_PASSWORD            // Password of the account
-					);
-				exit(0);
-			}
-			else if (serviceControl == "remove")
-			{
-				LogPrint(eLogInfo, "WinSVC: uninstalling ", SERVICE_NAME, " service");
-				UninstallService(SERVICE_NAME);
-				exit(0);
-			}
-			
-			if (isDaemon == 1)
-			{
-				LogPrint(eLogDebug, "Daemon: running as service");
-				I2PService service(SERVICE_NAME);
-				if (!I2PService::Run(service))
-				{
-					LogPrint(eLogError, "Daemon: Service failed to run w/err 0x%08lx\n", GetLastError());
-					exit(EXIT_FAILURE);
-				}
-				exit(EXIT_SUCCESS);
-			}
-			else
-				LogPrint(eLogDebug, "Daemon: running as user");
-
-			return true;
+            return Daemon_Singleton::init(argc, argv);
 		}
+
 		bool DaemonWin32::start()
 		{
 			setlocale(LC_CTYPE, "");
 			SetConsoleCP(1251);
 			SetConsoleOutputCP(1251);
 			setlocale(LC_ALL, "Russian");
+            if (!i2p::win32::StartWin32App ()) return false;
 
 			bool ret = Daemon_Singleton::start();
 			if (ret && IsLogToFile ())
-			{	
+			{
 				// TODO: find out where this garbage to console comes from
 				SetStdHandle(STD_OUTPUT_HANDLE, INVALID_HANDLE_VALUE);
 				SetStdHandle(STD_ERROR_HANDLE, INVALID_HANDLE_VALUE);
@@ -80,8 +40,14 @@ namespace i2p
 
 		bool DaemonWin32::stop()
 		{
+		    i2p::win32::StopWin32App ();
 			return Daemon_Singleton::stop();
 		}
+
+		void DaemonWin32::run ()
+        {
+            i2p::win32::RunWin32App ();
+        }
 	}
 }
 
