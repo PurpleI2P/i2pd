ff38a3bbfea4e195edf27847c5a351b97ff4b2c2
orignal <i2porignal@yandex.ru>
2016-06-15 10:17:02 -0400

    don't demonize

--- qt/i2pd_qt/main.cpp
+++ qt/i2pd_qt/main.cpp
@@ -3,20 +3,31 @@
 #include <stdlib.h>
 #include "../../Daemon.h"
 
+class DaemonQT: public i2p::util::Daemon_Singleton
+{
+	public:
+
+		static DaemonQT& Instance()
+		{
+			static DaemonQT instance;
+			return instance;
+		}
+};
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
 
 	int ret = -1;	
-	if (Daemon.init(argc, argv))
+	if (DaemonQT::Instance ().init(argc, argv))
 	{
-		if (Daemon.start())
+		if (DaemonQT::Instance ().start())
 		{
 			w.show();	
 			ret = a.exec();
 		}
-		Daemon.stop();
+		DaemonQT::Instance ().stop();
 	}
 
     return ret;
