da2c04f6815e76eb8c74d8b3d5709374deb188c7
hagen <hagen@mail.i2p>
2016-07-01 00:00:00 +0000

    * HTTPProxy.cpp : show created stream IDs in log

--- HTTPProxy.cpp
+++ HTTPProxy.cpp
@@ -296,7 +296,7 @@ namespace proxy {
 		}
 		if (Kill())
 			return;
-		LogPrint (eLogDebug, "HTTPProxy: Created new I2PTunnel stream");
+		LogPrint (eLogDebug, "HTTPProxy: Created new I2PTunnel stream, sSID=", stream->GetSendStreamID(), ", rSID=", stream->GetRecvStreamID());
 		auto connection = std::make_shared<i2p::client::I2PTunnelConnection>(GetOwner(), m_sock, stream);
 		GetOwner()->AddHandler (connection);
 		connection->I2PConnect (reinterpret_cast<const uint8_t*>(m_send_buf.data()), m_send_buf.length());
