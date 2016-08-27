7d03a41e3e8a12913bb281cadc3fc67a399052cc
Jeff Becker <ampernand@gmail.com>
2016-08-27 16:09:02 -0400

    try manual expiration of tags

--- Datagram.cpp
+++ Datagram.cpp
@@ -138,6 +138,8 @@ namespace datagram
       LogPrint(eLogInfo, "DatagramDestination: expiring idle session with ", ident.ToBase32());
       m_Sessions.erase(ident);
     }
+    m_LocalDestination->CleanupExpiredTags();
+    m_LocalDestination->CleanupUnconfirmedTags();
     ScheduleCleanup();
   }
   
