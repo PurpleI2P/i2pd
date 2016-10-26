143aaa2d28d939e598204035d7fabaee418f0ed7
hagen <hagen@mail.i2p>
2016-10-26 00:00:00 +0000

    * util.h : drop i2p::util::lexical_cast(), not used anymore (#314)

--- util.h
+++ util.h
@@ -5,9 +5,9 @@
 #include <string>
 #include <iostream>
 #include <boost/asio.hpp>
-#include <boost/lexical_cast.hpp>
 
 #ifdef ANDROID
+#include <boost/lexical_cast.hpp>
 namespace std
 {
 template <typename T>
@@ -22,18 +22,6 @@ namespace i2p
 {
 namespace util
 {
-	/**
-		 wrapper arround boost::lexical_cast that "never" fails
-	 */
-	template <typename T>
-	T lexical_cast(const std::string & str, const T fallback) {
-		try {
-			return boost::lexical_cast<T>(str);
-		} catch ( ... ) {
-			return fallback;
-		}
-	}
-
 	namespace net
 	{
 		int GetMTU (const boost::asio::ip::address& localAddress);
