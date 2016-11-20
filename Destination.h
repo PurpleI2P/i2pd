2a7748656744979f16e273d17027c836e31a33da
Jeff Becker <ampernand@gmail.com>
2016-11-20 09:30:46 -0500

    tabify

--- Destination.h
+++ Destination.h
@@ -137,7 +137,7 @@ namespace client
 			volatile bool m_IsRunning;
 			std::thread * m_Thread;	
 			boost::asio::io_service m_Service;
-		boost::asio::io_service::work m_Work;
+			boost::asio::io_service::work m_Work;
 			mutable std::mutex m_RemoteLeaseSetsMutex;
 			std::map<i2p::data::IdentHash, std::shared_ptr<i2p::data::LeaseSet> > m_RemoteLeaseSets;
 			std::map<i2p::data::IdentHash, std::shared_ptr<LeaseSetRequest> > m_LeaseSetRequests;
