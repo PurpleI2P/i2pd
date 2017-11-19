611c1a7502c729f3cbd37b3ba8e3b2d7d361cb82
hypnosis-i2p <hypnosis.i2p@gmail.com>
2017-11-19 19:26:36 +0800

    updated qt to build and to reflect some core changes like log dest

--- qt/i2pd_qt/SignatureTypeComboboxFactory.h
+++ qt/i2pd_qt/SignatureTypeComboboxFactory.h
@@ -70,13 +70,6 @@ public:
         addItem(signatureTypeCombobox, QApplication::translate("signatureTypeCombobox", "GOSTR3410_TC26_A_512_GOSTR3411_512", 0), SIGNING_KEY_TYPE_GOSTR3410_TC26_A_512_GOSTR3411_512); //10
         if(selectedSigType==SIGNING_KEY_TYPE_GOSTR3410_TC26_A_512_GOSTR3411_512){signatureTypeCombobox->setCurrentIndex(index);foundSelected=true;}
         ++index;
-        // TODO: remove later
-        addItem(signatureTypeCombobox, QApplication::translate("signatureTypeCombobox", "GOSTR3410_CRYPTO_PRO_A_GOSTR3411_256_TEST", 0), SIGNING_KEY_TYPE_GOSTR3410_CRYPTO_PRO_A_GOSTR3411_256_TEST); //65281
-        if(selectedSigType==SIGNING_KEY_TYPE_GOSTR3410_CRYPTO_PRO_A_GOSTR3411_256_TEST){signatureTypeCombobox->setCurrentIndex(index);foundSelected=true;}
-        ++index;
-        addItem(signatureTypeCombobox, QApplication::translate("signatureTypeCombobox", "GOSTR3410_TC26_A_512_GOSTR3411_512_TEST", 0), SIGNING_KEY_TYPE_GOSTR3410_TC26_A_512_GOSTR3411_512_TEST); //65282
-        if(selectedSigType==SIGNING_KEY_TYPE_GOSTR3410_TC26_A_512_GOSTR3411_512_TEST){signatureTypeCombobox->setCurrentIndex(index);foundSelected=true;}
-        ++index;
         if(!foundSelected){
             addItem(signatureTypeCombobox, QString::number(selectedSigType), selectedSigType); //unknown sigtype
             signatureTypeCombobox->setCurrentIndex(index);
