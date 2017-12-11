0f2d2156e6b4c75568a8c87606b1196cabff1948
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-12-11 22:34:48 +0300

    Don't use deprecated boost::asio::ssl::context ctor
    This was removed in boost 1.66, in prior versions the service
    argument was not used

--- libi2pd/Reseed.cpp
+++ libi2pd/Reseed.cpp
@@ -522,7 +522,7 @@ namespace data
 		boost::asio::io_service service;
 		boost::system::error_code ecode;
 
-		boost::asio::ssl::context ctx(service, boost::asio::ssl::context::sslv23);
+		boost::asio::ssl::context ctx(boost::asio::ssl::context::sslv23);
 		ctx.set_verify_mode(boost::asio::ssl::context::verify_none);
 		boost::asio::ssl::stream<boost::asio::ip::tcp::socket> s(service, ctx);
 
