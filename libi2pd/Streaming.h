06020b8f544ae09734992a47c95597e9998629c2
Jeff Becker <jeff@i2p.rocks>
2018-01-20 13:06:08 -0500

    re-enable packet pool

--- libi2pd/Streaming.h
+++ libi2pd/Streaming.h
@@ -276,8 +276,8 @@ namespace stream
 			/** set max connections per minute per destination */
 			void SetMaxConnsPerMinute(const uint32_t conns);
 
-			Packet * NewPacket () { return new Packet; }
-			void DeletePacket (Packet * p) { delete p; }
+			Packet * NewPacket () { return m_PacketsPool.Acquire(); }
+			void DeletePacket (Packet * p) { return m_PacketsPool.Release(p); }
 
 
 			void AcceptOnceAcceptor (std::shared_ptr<Stream> stream, Acceptor acceptor, Acceptor prev);
@@ -315,7 +315,7 @@ namespace stream
 			std::vector<i2p::data::IdentHash> m_Banned;
 			uint64_t m_LastBanClear;
 
-      //i2p::util::MemoryPool<Packet> m_PacketsPool;
+			i2p::util::MemoryPool<Packet> m_PacketsPool;
 			bool m_EnableDrop;
 
 		public:
