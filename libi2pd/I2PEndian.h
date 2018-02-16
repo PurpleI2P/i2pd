4988a32d3359b1be7fcb355dc53686a2540a250f
Jeff Becker <jeff@i2p.rocks>
2018-02-16 18:26:57 -0500

    make endian work with netbsd

--- libi2pd/I2PEndian.h
+++ libi2pd/I2PEndian.h
@@ -3,7 +3,7 @@
 #include <inttypes.h>
 #include <string.h>
 
-#if defined(__FreeBSD__)
+#if defined(__FreeBSD__) || defined(__NetBSD__)
 #include <sys/endian.h>
 #elif defined(__linux__) || defined(__FreeBSD_kernel__) || defined(__OpenBSD__)
 #include <endian.h>
