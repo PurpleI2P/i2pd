2bc0850b0fe363015401ecdadd422101ab7fab92
R4SAS <r4sas@i2pmail.org>
2021-01-13 21:06:12 +0300

    [android] add refresh on swipe in webconsole
    Signed-off-by: R4SAS <r4sas@i2pmail.org>

--- android/src/org/purplei2p/i2pd/WebConsoleActivity.java
+++ android/src/org/purplei2p/i2pd/WebConsoleActivity.java
@@ -2,14 +2,18 @@ package org.purplei2p.i2pd;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.Objects;
 
 public class WebConsoleActivity extends Activity {
+    private WebView webView;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +22,43 @@ public class WebConsoleActivity extends Activity {
 
         Objects.requireNonNull(getActionBar()).setDisplayHomeAsUpEnabled(true);
 
-        final WebView webView = findViewById(R.id.webview1);
+        webView = (WebView) findViewById(R.id.webview1);
         webView.setWebViewClient(new WebViewClient());
 
         final WebSettings webSettings = webView.getSettings();
         webSettings.setBuiltInZoomControls(true);
         webSettings.setJavaScriptEnabled(false);
         webView.loadUrl("http://127.0.0.1:7070"); // TODO: instead 7070 I2Pd....HttpPort
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                        webView.reload();
+                    }
+                }, 1000);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id==android.R.id.home) {
+        if (id == android.R.id.home) {
             finish();
             return true;
         }
