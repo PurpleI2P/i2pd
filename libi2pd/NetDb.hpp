541464b7057095a220db0d094fdaef16d8191fd5
orignal <i2porignal@yandex.ru>
2021-08-26 15:13:58 -0400

    don't delete floodfill if number of remaining floodfills is less than minimal

--- libi2pd/NetDb.hpp
+++ libi2pd/NetDb.hpp
@@ -36,6 +36,7 @@ namespace i2p
 namespace data
 {
 	const int NETDB_MIN_ROUTERS = 90;
+	const int NETDB_MIN_FLOODFILLS = 5;
 	const int NETDB_FLOODFILL_EXPIRATION_TIMEOUT = 60 * 60; // 1 hour, in seconds
 	const int NETDB_INTRODUCEE_EXPIRATION_TIMEOUT = 65 * 60;
 	const int NETDB_MIN_EXPIRATION_TIMEOUT = 90 * 60; // 1.5 hours
