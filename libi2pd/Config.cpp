ec5c13a95ecd6d31f2a49477d20ae2bca1cf711b
orignal <i2porignal@yandex.ru>
2023-03-08 18:38:49 -0500

    add back i2p.novg.net with new certificate

--- libi2pd/Config.cpp
+++ libi2pd/Config.cpp
@@ -217,8 +217,9 @@ namespace config {
 				"https://reseed.i2pgit.org/,"
 				"https://banana.incognet.io/,"
 				"https://reseed-pl.i2pd.xyz/,"
-				"https://www2.mk16.de/",
-			    "https://i2p.ghativega.in/"
+				"https://www2.mk16.de/,"
+			    "https://i2p.ghativega.in/,"
+			    "https://i2p.novg.net/"
 			),                                                            "Reseed URLs, separated by comma")
 			("reseed.yggurls", value<std::string>()->default_value(
 				"http://[324:71e:281a:9ed3::ace]:7070/,"
