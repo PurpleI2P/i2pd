c53ad2012ceb9c7489c97378e86500332d8edbac
r4sas <r4sas@i2pmail.org>
2023-09-06 20:26:46 +0000

    [cpu] use cpuid on gcc < 5
    Signed-off-by: r4sas <r4sas@i2pmail.org>

--- libi2pd/CPU.cpp
+++ libi2pd/CPU.cpp
@@ -9,12 +9,16 @@
 #include "CPU.h"
 #include "Log.h"
 
-#if defined(_MSC_VER)
-#include <intrin.h>
-
 #ifndef bit_AES
 	#define bit_AES (1 << 25)
 #endif
+
+#if (defined(__GNUC__) && __GNUC__ < 5)
+	#include <cpuid.h>
+#endif
+
+#ifdef _MSC_VER
+	#include <intrin.h>
 #endif
 
 namespace i2p
@@ -26,18 +30,18 @@ namespace cpu
 	inline bool cpu_support_aes()
 	{
 #if (defined(_M_AMD64) || defined(__x86_64__)) || (defined(_M_IX86) || defined(__i386__))
-#if defined(_MSC_VER)
-		int cpu_info[4];
-		__cpuid(cpu_info, 1);
-		return ((cpu_info[2] & bit_AES) != 0);
+#if (defined(__GNUC__) && __GNUC__ > 4)
+		__builtin_cpu_init();
+		return __builtin_cpu_supports("aes");
 #elif defined(__clang__)
 #if __clang_major__ >= 6
 		__builtin_cpu_init();
 #endif
 		return __builtin_cpu_supports("aes");
-#elif defined(__GNUC__)
-		__builtin_cpu_init();
-		return __builtin_cpu_supports("aes");
+#elif (defined(_MSC_VER) || (defined(__GNUC__) && __GNUC__ < 5))
+		int cpu_info[4];
+		__cpuid(cpu_info, 1);
+		return ((cpu_info[2] & bit_AES) != 0);
 #else
 		return false;
 #endif
