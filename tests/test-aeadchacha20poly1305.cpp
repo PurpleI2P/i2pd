fc16a70f7bad5befe972f0d70c714e705feb77a5
orignal <i2porignal@yandex.ru>
2025-01-02 18:30:16 -0500

    use AEADChaCha20Poly1305Encryptor and AEADChaCha20Poly1305Decryptor for test

--- tests/test-aeadchacha20poly1305.cpp
+++ tests/test-aeadchacha20poly1305.cpp
@@ -43,19 +43,20 @@ uint8_t encrypted[114] =
 int main ()
 {
 	uint8_t buf[114+16];
+    i2p::crypto::AEADChaCha20Poly1305Encryptor encryptor;
 	// test encryption
-	i2p::crypto::AEADChaCha20Poly1305 ((uint8_t *)text, 114, ad, 12, key, nonce, buf, 114 + 16, true);
+	encryptor.Encrypt ((uint8_t *)text, 114, ad, 12, key, nonce, buf, 114 + 16);
 	assert (memcmp (buf, encrypted, 114) == 0);
 	assert (memcmp (buf + 114, tag, 16) == 0);
 	// test decryption
 	uint8_t buf1[114];
-	assert (i2p::crypto::AEADChaCha20Poly1305 (buf, 114, ad, 12, key, nonce, buf1, 114, false));
+    i2p::crypto::AEADChaCha20Poly1305Decryptor decryptor;
+	assert (decryptor.Decrypt (buf, 114, ad, 12, key, nonce, buf1, 114));
 	assert (memcmp (buf1, text, 114) == 0);
 	// test encryption of multiple buffers
 	memcpy (buf, text, 114);
 	std::vector<std::pair<uint8_t*, std::size_t> > bufs{ std::make_pair (buf, 20), std::make_pair (buf + 20, 10), std::make_pair (buf + 30, 70), std::make_pair (buf + 100, 14) };
-    i2p::crypto::AEADChaCha20Poly1305Encryptor encryptor;
 	encryptor.Encrypt (bufs, key, nonce, buf + 114);
-	i2p::crypto::AEADChaCha20Poly1305 (buf, 114, nullptr, 0, key, nonce, buf1, 114, false);
+	decryptor.Decrypt (buf, 114, nullptr, 0, key, nonce, buf1, 114);
 	assert (memcmp (buf1, text, 114) == 0);
 }
