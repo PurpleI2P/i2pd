a95b1ce0612f50067d6fcdef7ed28f7ce984a862
orignal <i2porignal@yandex.ru>
2025-11-29 11:14:31 -0500

    removing the window size growth rate limit

--- libi2pd/Streaming.h
+++ libi2pd/Streaming.h
@@ -62,7 +62,7 @@ namespace stream
 	const int INITIAL_WINDOW_SIZE = 10;
 	const int MIN_WINDOW_SIZE = 3;
 	const int MAX_WINDOW_SIZE = 512;
-	const int MAX_WINDOW_SIZE_INC_PER_RTT = 12;
+	const int MAX_WINDOW_SIZE_INC_PER_RTT = 512; // TODO: remove
 	const double RTT_EWMA_ALPHA = 0.1;
 	const double SLOWRTT_EWMA_ALPHA = 0.02;
 	const double PREV_SPEED_KEEP_TIME_COEFF = 0.2; // 0.1 - 1 // how long will the window size stay around the previous drop level, less is longer
