2f490e6262b2eb778c3850e2f80d4c3800b5e23e
acetone <63557806+freeacetone@users.noreply.github.com>
2026-07-21 20:43:54 +0300

    fix data race on intermediate queue in SSU2 session termination

--- libi2pd/SSU2Session.cpp
+++ libi2pd/SSU2Session.cpp
@@ -292,8 +292,11 @@ namespace transport
 #if OPENSSL_PQ
 			m_PQKeys.reset (nullptr);
 #endif
-			if (!m_IntermediateQueue.empty ())
-				m_SendQueue.splice (m_SendQueue.end (), m_IntermediateQueue);
+			{
+				std::lock_guard<std::mutex> l(m_IntermediateQueueMutex);
+				if (!m_IntermediateQueue.empty ())
+					m_SendQueue.splice (m_SendQueue.end (), m_IntermediateQueue);
+			}
 			for (auto& it: m_SendQueue)
 				it->Drop ();
 			m_SendQueue.clear ();
